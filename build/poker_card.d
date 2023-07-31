60c75326065c857e70312bddadaed75623be308a
Rodrigo Casale <popobasura3@hotmail.com>
2023-07-31 14:31:51 -0600

    build files

---
+++ build/poker_card.d
@@ -0,0 +1,62 @@
+poker_card.o: \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/src/poker_card.cpp \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sstream.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_common.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/../hw/include/bn_hw_common.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string_view.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_assert.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_type_traits.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_assert.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_doxygen.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_istring_base.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_algorithm.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_functional.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_alignment.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_utility.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_compare.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_iterator.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_card.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_random.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_limits.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_math.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sin_lut.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_reciprocal_lut.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_rule_of_three_approximation.h
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sstream.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_common.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/../hw/include/bn_hw_common.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string_view.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_assert.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_type_traits.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_assert.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_doxygen.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_istring_base.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_algorithm.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_functional.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_alignment.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_utility.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_compare.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_iterator.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_card.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_random.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_limits.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_math.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sin_lut.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_reciprocal_lut.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_rule_of_three_approximation.h:
---
+++ build/poker_hand.d
@@ -0,0 +1,62 @@
+poker_hand.o: \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/src/poker_hand.cpp \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_hand.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_assert.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_type_traits.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_common.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/../hw/include/bn_hw_common.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_assert.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_doxygen.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sstream.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_istring_base.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_algorithm.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_functional.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_alignment.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_utility.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_compare.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_iterator.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_card.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_random.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_limits.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_math.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array_fwd.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sin_lut.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_reciprocal_lut.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_rule_of_three_approximation.h \
+ /Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_pocket.h
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_hand.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_assert.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_type_traits.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_common.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/../hw/include/bn_hw_common.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_assert.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_config_doxygen.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sstream.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_string_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_istring_base.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_algorithm.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_functional.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_alignment.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_utility.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_compare.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_iterator.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_vector_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_card.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_random.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_fixed.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_limits.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_math.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_array_fwd.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_sin_lut.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_reciprocal_lut.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/butano/include/bn_rule_of_three_approximation.h:
+/Users/rodrigocasale/Documents/proyects/gba_dev/butano/games/poker/include/poker_pocket.h:
